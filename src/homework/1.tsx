2e1f16052e34973bbde2b92392d36ed734f9a774
olena-nikitchenko <helnikitka@gmail.com>
2023-11-02 19:56:40 +0200

    solved task1

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,40 +1,50 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
+
+type Props = {
+	children: React.ReactNode;
+	onContentEndVisible: () => void;
+};
+type Options = {
+	rootMargin: string;
+	threshold: number;
+	root?: Element | null;
+};
 
 // Опишіть Props
 export function Observer({ children, onContentEndVisible }: Props) {
-  // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+	// Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
+	const endContentRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0,
-      root: null,
-    };
+	useEffect(() => {
+		// Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
+		const options: Options = {
+			rootMargin: "0px",
+			threshold: 1.0,
+			root: null,
+		};
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.intersectionRatio > 0) {
-          onContentEndVisible();
-          observer.disconnect();
-        }
-      });
-    }, options);
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.intersectionRatio > 0) {
+					onContentEndVisible();
+					observer.disconnect();
+				}
+			});
+		}, options);
 
-    if (endContentRef.current) {
-      observer.observe(endContentRef.current);
-    }
+		if (endContentRef.current) {
+			observer.observe(endContentRef.current);
+		}
 
-    return () => {
-      observer.disconnect();
-    };
-  }, [onContentEndVisible]);
+		return () => {
+			observer.disconnect();
+		};
+	}, [onContentEndVisible]);
 
-  return (
-    <div>
-      {children}
-      <div ref={endContentRef} />
-    </div>
-  );
+	return (
+		<div>
+			{children}
+			<div ref={endContentRef} />
+		</div>
+	);
 }
